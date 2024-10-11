8eadcb27dcbbe01840e7ea6cd92a185fa444c5cd
cmanlh <cmanlh@163.com>
2024-10-11 23:03:34 +0800

    adjust lib path

--- build/RuntimeModifier/Runtime.bat
+++ build/RuntimeModifier/Runtime.bat
@@ -1,5 +1,5 @@
 set projectName=RuntimeModifier
 set programName=Runtime
 mkdir .\output\%projectName%
-gcc -g -o .\output\%projectName%\%programName%.exe .\src\%projectName%\%programName%.c .\lib\raylib\libraylib.a -lgdi32 -lwinmm
+gcc -g -o .\output\%projectName%\%programName%.exe .\src\%projectName%\%programName%.c .\lib\raylib\windows\libraylib.a -lgdi32 -lwinmm
 .\output\%projectName%\%programName%.exe
